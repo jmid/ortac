c9428523b645e835441bb8635f3c7db9cdf07438
Jan Midtgaard <mail@janmidtgaard.dk>
2023-09-29 15:07:32 +0200

    First version with wrong Not_found exception

---
+++ plugins/qcheck-stm/test/queue.mli
@@ -0,0 +1,18 @@
+type !'a t
+(*@ mutable model contents : 'a list *)
+
+val create : unit -> 'a t
+(*@ q = create ()
+    ensures q.contents = [] *)
+
+val add : 'a -> 'a t -> unit
+(*@ add a q
+    modifies q
+    ensures q.contents = a :: old q.contents *)
+
+val take : 'a t -> 'a
+(*@ a = take q
+    modifies q
+    raises Not_found -> q.contents = []
+    ensures q.contents = List.tl (old q.contents)
+    ensures a = List.hd q.contents *)
